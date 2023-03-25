2ff326ce49fcf67c3fda23e85c9ff0a56be1db08
Davidson <davidsonquaresma@hotmail.com>
2023-03-25 11:18:10 -0300

    fix: add schemas null

--- ClientBookApi/src/schemas/contacts/createContact.schemas.ts
+++ ClientBookApi/src/schemas/contacts/createContact.schemas.ts
@@ -3,8 +3,8 @@ import { z } from "zod";
 const createContactsSchemas = z.object({
   name: z.string().max(50).min(3).trim(),
   email: z.string().max(50).email().trim(),
-  secondEmail: z.string().max(50).email().trim().optional(),
-  secondPhone: z.string().max(20).optional(),
+  secondEmail: z.string().max(50).email().trim().nullable().optional(),
+  secondPhone: z.string().max(20).nullable().optional(),
   phone: z.string().max(20),
 });
 
