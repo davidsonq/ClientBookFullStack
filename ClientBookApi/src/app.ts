a7f3d70bdd0b670e636610639a0280c69e41aa07
Davidson <davidsonquaresma@hotmail.com>
2023-03-25 11:16:00 -0300

    fix: schemas, add cors

--- ClientBookApi/src/app.ts
+++ ClientBookApi/src/app.ts
@@ -1,6 +1,7 @@
 import "reflect-metadata";
 import "express-async-errors";
 import express, { json } from "express";
+import cors from "cors";
 import handleError from "./errors/errors";
 import userRoutes from "./routes/users/users.routes";
 import contactRoutes from "./routes/contacts/contacts.routes";
@@ -9,6 +10,8 @@ import sessionRoutes from "./routes/sessions/sessions.routes";
 const app = express();
 
 app.use(json());
+
+app.use(cors());
 
 app.use("/users", userRoutes);
 app.use("/login", sessionRoutes);
